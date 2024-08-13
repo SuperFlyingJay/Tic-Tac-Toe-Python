c7f522ff6d885aa0754f9e857af5963b6cf23170
SuperFlyingJay <flying_j@rocketmail.com>
2024-08-13 15:37:54 +0000

    Add insturctions to print the game board

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 # Tic Tac Toe - Console-Based
 #
-# This program will allow the user to play Tic Tac Toe against the computer. The board will be drawn in text-characters on the screen.
+# This program will allow the user to play Tic Tac Toe against the computer. 
+#The board will be drawn in text-characters on the screen.
 #
 # Author: I aint givin you my name
 # Date Created: August 12, 2024
 # Last Modified: August 12, 2024
+
+print("   |   |   ")
+print(" 1 | 2 | 3 ")
+print("   |   |   ")
+print("---+---+---")
+print("   |   |   ")
+print(" 4 | 5 | 6 ")
+print("   |   |   ")
+print("---+---+---")
+print("   |   |   ")
+print(" 7 | 8 | 9 ")
+print("   |   |   ")
