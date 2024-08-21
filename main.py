dfba47cf9778b2612999669fd243610947c415d5
SuperFlyingJay <flying_j@rocketmail.com>
2024-08-21 18:18:35 +0000

    Add a parameter to draw_board so we can pass the board model data; add a get_symbol function to retrieve the symbol for a specific square on the board

--- main.py
+++ main.py
@@ -5,25 +5,32 @@
 #
 # Author: I aint givin you my name
 # Date Created: August 12, 2024
-# Last Modified: August 12, 2024
+# Last Modified: August 21, 2024
 
 EMPTY = "empty"
-board = [EMPTY, EMPTY, EMPTY,
+board = [
+         EMPTY, EMPTY, EMPTY,
          EMPTY, EMPTY, EMPTY,
          EMPTY, EMPTY, EMPTY
-]
+        ]
 
-def draw_board():
+def get_symbol(the_board, square_number):
+         symbol = the_board[square_number - 1]
+         if symbol == EMPTY:
+                  return square_number
+         return symbol
+                  
+def draw_board(the_board):
   print("   |   |   ")
-  print(" 1 | 2 | 3 ")
+  print(" {} | {} | {} ".format(get_symbol(the_board, 1), get_symbol(the_board, 2), get_symbol(the_board, 3)))
   print("   |   |   ")
   print("---+---+---")
   print("   |   |   ")
-  print(" 4 | 5 | 6 ")
+  print(" {} | {} | {} ".format(get_symbol(the_board, 4), get_symbol(the_board, 5), get_symbol(the_board, 6)))
   print("   |   |   ")
   print("---+---+---")
   print("   |   |   ")
-  print(" 7 | 8 | 9 ")
+  print(" {} | {} | {} ".format(get_symbol(the_board, 7), get_symbol(the_board, 8), get_symbol(the_board, 9)))
   print("   |   |   ")
 
-draw_board()
+draw_board(board         )
